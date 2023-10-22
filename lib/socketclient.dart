604c25e47e9f6bbad6dbc1f7b63b1d08f2bee12d
sanjay14073 <103362239+sanjay14073@users.noreply.github.com>
2023-10-22 22:06:27 +0530

    Update socketclient.dart

--- lib/socketclient.dart
+++ lib/socketclient.dart
@@ -1,12 +1,13 @@
 import 'package:socket_io_client/socket_io_client.dart' as io;
 //singleton class pattern why this? to create only a single instance of the class multiple instances not supported
+//Please replace your IP address the _internal is a internal constructor 
 class Client{
   io.Socket?socket;
   static Client? _instance;
   //private constructor
   Client._internal(){
     //print("request sent");
-    socket=io.io('http://192.168.1.40:3000',<String,dynamic>{
+    socket=io.io('http://0.0.0.0:3000',<String,dynamic>{
       'transports':['websocket'],
       'autoConnect':false,
     });
